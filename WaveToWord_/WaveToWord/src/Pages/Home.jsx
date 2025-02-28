ce6b8e5092adac893962e9c76b1f6a1e0bc3f64d
Rishitha Reddy Shaganti <23wh1a05b6@bvrithyderabad.edu.in>
2025-02-28 21:41:37 +0530

    created 2 files

--- WaveToWord_/WaveToWord/src/Pages/Home.jsx
+++ WaveToWord_/WaveToWord/src/Pages/Home.jsx
@@ -9,9 +9,10 @@ function Home() {
       {/* Navbar */}
       <nav className="navbar">
         <div className="nav-links">
+          <Link to="/about" className="nav-button">Home</Link>
           <Link to="/settings" className="nav-button">Settings</Link>
           <Link to="/qna" className="nav-button">Q&A</Link>
-          <Link to="/about" className="nav-button">Home</Link>
+          
         </div>
       </nav>
 
